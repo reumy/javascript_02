25b86e784d0ad8cf287a50592eb1210086c6b718
reumy <reuoomy@gmail.com>
2018-09-23 01:52:03 +0900

    Update script.js

--- 과제/06/js/script.js
+++ 과제/06/js/script.js
@@ -1,27 +1,27 @@
 class TV {
-	constructor(){
-	  let _company = '삼성';
-	  this.inch = '50';
-	  this.model = 'UHD50';
+  constructor(){
+    let _company = '삼성';
+    this.inch = '50';
+    this.model = 'UHD50';
 
-		this.getCompany = () => {
-			console.log(_company);
-		}
+    this.getCompany = () => {
+      console.log(_company);
+    }
 
-		this.setCompany = (company) => {
-	    _company = company
-	  }
-	}
+    this.setCompany = (company) => {
+      _company = company
+    }
+  }
 }
 
 class AppTV extends TV {
-	constructor(){
-		super();
-		this.inch = '60';
-	}
-	app (){
-		// 앱기능 추가
-	}
+  constructor(){
+    super();
+    this.inch = '60';
+  }
+  app (){
+    // 앱기능 추가
+  }
 }
 
 var tv = new TV();
